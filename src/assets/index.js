a73f868ad46f748d81284bed7cb91171af4f6386
Ujun Junaedi <jujunjuna189@gmail.com>
2024-12-29 20:37:15 +0700

    Update

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,17 @@
+import avatarUser from "./avatar/avatarUser.png";
+import avatarAdmin from "./avatar/avatarAdmin.png";
+import avatarSuperAdmin from "./avatar/avatarSuperAdmin.png";
 import imgKantorSdirsen from "./image/kantor_sdirsen.png";
 import imgBg from "./image/bg.png";
 import logLogo from "./logo/logo.png";
 import icSoldier from "./icon/icSoldier.png";
 
-export { imgKantorSdirsen, imgBg, logLogo, icSoldier };
+export { 
+    avatarUser,
+    avatarAdmin,
+    avatarSuperAdmin,
+    imgKantorSdirsen, 
+    imgBg, 
+    logLogo, 
+    icSoldier 
+};
--- src/components/moleculs/navbar/index.js
+++ src/components/moleculs/navbar/index.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { logLogo } from "../../../assets";
+import { avatarAdmin, avatarSuperAdmin, avatarUser, logLogo } from "../../../assets";
 import { UseAuthContext } from "../../../contexts/auth/AuthContext";
 import MenuProfileModal from "../modal/MenuProfileModal";
+import { getLocalUser } from "../../../utils";
 
 const Navbar = (props) => {
     const navigation = useNavigate();
@@ -17,6 +18,18 @@ const Navbar = (props) => {
         setElement(undefined);
     }
 
+    const renderAvatar = () => {
+        var role = getLocalUser()?.auth?.user?.role_id;
+
+        if(Number(role) === 1){
+            return avatarAdmin;
+        }else if(Number(role) === 2){
+            return avatarUser;
+        } else if(Number(role) === 3){
+            return avatarSuperAdmin;
+        }
+    }
+
     return (
         <nav className="bg-white py-3 px-7 shadow-md z-10 flex justify-between items-center">
             <div className="text-lg font-semibold cursor-pointer flex items-center gap-2" onClick={() => navigation('/dashboard')}>
@@ -27,7 +40,9 @@ const Navbar = (props) => {
             </div>
             <div className="flex items-center gap-3 cursor-pointer" onClick={() => navigation('/profile')} onMouseOver={() => onSetElement()} onMouseLeave={() => onEmptyElement()}>
                 <span className="font-semibold">{(user?.auth?.user?.name && user?.auth?.user?.name.split(' ')[0]) ?? 'Anonim'}</span>
-                <div className="w-8 h-8 rounded-full bg-slate-200"></div>
+                <div className="w-8 h-8 rounded-full bg-slate-200">
+                    <img src={renderAvatar()} alt="Profile"/>
+                </div>
             </div>
             {element}
         </nav>
